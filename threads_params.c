b2070ff76919fd0388af2b3f330f0b9d4951dae4
snaipasomoh <40523782+snaipasomoh@users.noreply.github.com>
2021-12-01 14:06:55 +0300

    Add files via upload

--- threads_params.c
+++ threads_params.c
@@ -11,62 +11,62 @@ extern void led_off(void);
 void
 task_0(void *args)
 {
-	unsigned selfPrio;
-	// Pointer to thread can be obtained from it's task with thread_self.
-	// Some thread params can be obtained with thread_get_params.
-	fx_thread_get_params(fx_thread_self(), FX_THREAD_PARAM_PRIO, &selfPrio);
-	printf("Hello from task_0 with prio = %u\n\r", selfPrio);
-	fx_thread_exit();
+    unsigned selfPrio;
+    // Pointer to thread can be obtained from it's task with thread_self.
+    // Some thread params can be obtained with thread_get_params.
+    fx_thread_get_params(fx_thread_self(), FX_THREAD_PARAM_PRIO, &selfPrio);
+    printf("Hello from task_0 with prio = %u\n\r", selfPrio);
+    fx_thread_exit();
 }
 
 void
 task_1(void *args)
 {
-	unsigned selfPrio;
-	fx_thread_get_params(fx_thread_self(), FX_THREAD_PARAM_PRIO, &selfPrio);
-	printf("Hello from task_1 with prio = %u\n\r", selfPrio);
-	fx_thread_exit();
+    unsigned selfPrio;
+    fx_thread_get_params(fx_thread_self(), FX_THREAD_PARAM_PRIO, &selfPrio);
+    printf("Hello from task_1 with prio = %u\n\r", selfPrio);
+    fx_thread_exit();
 }
 
 void
 task_main(void *args)
 {
-	// Creating thread and it's stack.
-	static fx_thread_t thread;
-	static uint32_t stack[0x200 / sizeof(uint32_t)];
-	
-	// Initializing thread with task_0 and priority level 10.
-	fx_thread_init(&thread, task_0, NULL, 10, stack, sizeof(stack), false);
-	// Waiting for thread's task ending.
-	fx_thread_join(&thread);
+    // Creating thread and it's stack.
+    static fx_thread_t thread;
+    static uint32_t stack[0x200 / sizeof(uint32_t)];
+    
+    // Initializing thread with task_0 and priority level 10.
+    fx_thread_init(&thread, task_0, NULL, 10, stack, sizeof(stack), false);
+    // Waiting for thread's task ending.
+    fx_thread_join(&thread);
 
-	// Thread structure and it's stack can be reused but deinit must be called
-	// before.
-	fx_thread_deinit(&thread);
-	// Reinitializing thread with other task and priority level.
-	fx_thread_init(&thread, task_1, NULL, 9, stack, sizeof(stack), false);
-	fx_thread_join(&thread);
+    // Thread structure and it's stack can be reused but deinit must be called
+    // before.
+    fx_thread_deinit(&thread);
+    // Reinitializing thread with other task and priority level.
+    fx_thread_init(&thread, task_1, NULL, 9, stack, sizeof(stack), false);
+    fx_thread_join(&thread);
 
-	fx_thread_deinit(&thread);
-	fx_thread_init(&thread, task_0, NULL, 10, stack, sizeof(stack), false);
-	// Priority level can be changed with set_params after initializing thread.
-	fx_thread_set_params(&thread, FX_THREAD_PARAM_PRIO, 7);
-	fx_thread_join(&thread);
+    fx_thread_deinit(&thread);
+    fx_thread_init(&thread, task_0, NULL, 10, stack, sizeof(stack), false);
+    // Priority level can be changed with set_params after initializing thread.
+    fx_thread_set_params(&thread, FX_THREAD_PARAM_PRIO, 7);
+    fx_thread_join(&thread);
 
-	fx_thread_exit();
+    fx_thread_exit();
 }
 
 void
 fx_app_init(void)
 {
-	static fx_thread_t thread_main;
-	static uint32_t stack_main[0x400 / sizeof(uint32_t)];
-	fx_thread_init(&thread_main, task_main, NULL, 6, stack_main, 
-	               sizeof(stack_main), false);
+    static fx_thread_t thread_main;
+    static uint32_t stack_main[0x400 / sizeof(uint32_t)];
+    fx_thread_init(&thread_main, task_main, NULL, 6, stack_main, 
+                   sizeof(stack_main), false);
 }
 
 void
 fx_intr_handler(void)
 {
-	;
+    ;
 }
--- threads_terminate.c
+++ threads_terminate.c
@@ -14,12 +14,12 @@ extern void led_off(void);
 void
 task_0(void* args)
 {
-	printf("Task_0 started\n\r");
-	for (;;)
+    printf("Task_0 started\n\r");
+    for (;;)
     {
-		led_on();
-		fx_thread_yield();
-	}
+        led_on();
+        fx_thread_yield();
+    }
 }
 
 // This task starts first.
@@ -31,56 +31,56 @@ task_0(void* args)
 void
 task_1(void* target)
 {
-	printf("Task_1 started\n\r");
+    printf("Task_1 started\n\r");
 
     // Going into suspended mode.
-	fx_thread_suspend();
+    fx_thread_suspend();
 
-	printf("Task_1 restarted\n\r");
+    printf("Task_1 restarted\n\r");
 
     // Killing thread_0.
-	fx_thread_terminate(target);
-	led_off();
+    fx_thread_terminate(target);
+    led_off();
 
-	fx_thread_exit();
+    fx_thread_exit();
 }
 
 void
 task_main(void* args)
 {
     // Creating threads and their stacks.
-	static fx_thread_t thread_0;
-	static fx_thread_t thread_1;
-	static uint32_t stack_0[0x200 / sizeof(uint32_t)];
-	static uint32_t stack_1[0x200 / sizeof(uint32_t)];
-	
+    static fx_thread_t thread_0;
+    static fx_thread_t thread_1;
+    static uint32_t stack_0[0x200 / sizeof(uint32_t)];
+    static uint32_t stack_1[0x200 / sizeof(uint32_t)];
+    
     // Initializing threads in suspended mode.
     // Pointer to thread_0 is passed to thread_1 as argument.
-	fx_thread_init(&thread_0, task_0, NULL, 10, stack_0, sizeof(stack_0), true);
-	fx_thread_init(&thread_1, task_1, &thread_0, 10, stack_1, sizeof(stack_1), true);
+    fx_thread_init(&thread_0, task_0, NULL, 10, stack_0, sizeof(stack_0), true);
+    fx_thread_init(&thread_1, task_1, &thread_0, 10, stack_1, sizeof(stack_1), true);
 
     // Starting threads in other order than they were initialized.
-	fx_thread_resume(&thread_1);
-	fx_thread_resume(&thread_0);
+    fx_thread_resume(&thread_1);
+    fx_thread_resume(&thread_0);
 
     // Waiting for 5000 ticks and then resuming thread_1.
-	fx_thread_sleep(5000);
-	fx_thread_resume(&thread_1);
+    fx_thread_sleep(5000);
+    fx_thread_resume(&thread_1);
 
-	fx_thread_exit();
+    fx_thread_exit();
 }
 
 void
 fx_app_init(void)
 {
-	static fx_thread_t thread_main;
+    static fx_thread_t thread_main;
     static uint32_t stack_main[0x400 / sizeof(uint32_t)];
-	fx_thread_init(&thread_main, task_main, NULL, 9, stack_main, 
+    fx_thread_init(&thread_main, task_main, NULL, 9, stack_main, 
                    sizeof(stack_main), false);
 }
 
 void
 fx_intr_handler(void)
 {
-	;
+    ;
 }
